9e191adc442f13c7f2cfb95e821d65bd292b55a4
Meghana <lk496@cornell.edu>
2024-11-21 21:33:08 -0500

    latest deployment structure

--- backend/app.py
+++ backend/app.py
@@ -409,5 +409,6 @@ def upload_file():
     return jsonify({'message': 'File uploaded successfully', 'file_path': file_path})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     
